107282a043da970e6e90f453b6317ded9fd3a3d5
Mohammed Rabil <rabil@techie.com>
2024-04-15 16:17:26 +0530

    Update ABI configuration in app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,25 +50,25 @@ android {
 
     splits {
 
-        // Configures multiple APKs based on ABI.
-        abi {
+    // Configures multiple APKs based on ABI.
+    abi {
 
-        // Enables building multiple APKs per ABI.
-        enable true
+      // Enables building multiple APKs per ABI.
+      isEnable = true
 
-        // By default all ABIs are included, so use reset() and include to specify that you only
-        // want APKs for x86 and x86_64.
+      // By default all ABIs are included, so use reset() and include to specify that you only
+      // want APKs for x86 and x86_64.
 
-        // Resets the list of ABIs for Gradle to create APKs for to none.
-        reset()
+      // Resets the list of ABIs for Gradle to create APKs for to none.
+      reset()
 
-        // Specifies a list of ABIs for Gradle to create APKs for.
-        include "x86", "x86_64", "armeabi-v7a", "arm64-v8a"
+      // Specifies a list of ABIs for Gradle to create APKs for.
+      include("x86", "x86_64", "armeabi-v7a", "arm64-v8a")
 
-        // Specifies that you don't want to also generate a universal APK that includes all ABIs.
-        universalApk true
-        }
+      // Specifies that you don't want to also generate a universal APK that includes all ABIs.
+      isUniversalApk = true
     }
+  }
 
 }
 
